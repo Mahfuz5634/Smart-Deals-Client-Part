c94390054b18b4ca265b85cf1a82aa7ed50d214c
Mahfuz <mahfuzru5634@gmail.com>
2025-11-03 22:32:52 +0600

    added personal n ote

---
+++ src/Note.jsx
@@ -0,0 +1,15 @@
+/*
+----------Firebase AccesToken-------------------->
+
+fetch("address",{
+  headers:{
+  authorization:`Bearer${user.accesToken}`
+
+  }
+})
+
+---------------Server Site AccesToken Acces---------------->
+const token = req.body.authorization;
+
+
+*/
